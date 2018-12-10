9f25c29bb31c7139ed716d8ac166af3ce9e0e0a4
mayk jony <drum.ayk@gmail.com>
2018-12-11 00:15:53 +0300

    first commit

---
+++ x010_testing/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/progayk/go-programmingting/01"
+)
+
+func main() {
+	avg := mjmath.Avarage([]float64{1, 2})
+	fmt.Println("hayde gorelim avarage sonucunu", avg)
+	
+}
